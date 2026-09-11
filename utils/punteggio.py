25ab4c37a3a5bf8f30a0d57564180f49a70c89e8
frap998-bit <frap998@gmail.com>
2026-09-11 11:44:33 +0200

    Update punteggio.py

--- utils/punteggio.py
+++ utils/punteggio.py
@@ -5,10 +5,27 @@ def inizializza_punteggio():
     if "punteggio" not in st.session_state:
         st.session_state.punteggio = 0
 
+    if "giorni_completati" not in st.session_state:
+        st.session_state.giorni_completati = set()
 
-def aggiungi_punti(punti):
-    st.session_state.punteggio += punti
+
+def aggiungi_punti(giorno, punti):
+    inizializza_punteggio()
+
+    # I punti vengono assegnati una sola volta
+    if giorno not in st.session_state.giorni_completati:
+        st.session_state.punteggio += punti
+        st.session_state.giorni_completati.add(giorno)
+        return True
+
+    return False
 
 
 def get_punteggio():
+    inizializza_punteggio()
     return st.session_state.punteggio
+
+
+def giorno_completato(giorno):
+    inizializza_punteggio()
+    return giorno in st.session_state.giorni_completati
