7e5c7793bae4b6daae5272067b3c4ef7840b5cfc
frap998-bit <frap998@gmail.com>
2026-09-12 15:19:28 +0200

    Update giorno20.py

--- giorni/giorno20.py
+++ giorni/giorno20.py
@@ -59,7 +59,7 @@ def mostra_gioco():
         {
             "numero": 4,
             "domanda": "Il nostro motto? (Sono due parole)",
-            "lettere": 12,
+            "lettere": 11,
             "soluzione": "PIANO PIANO"
         }
     ]
